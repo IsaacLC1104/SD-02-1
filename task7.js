d6f9cf7543553c9cf62618345ce685ce63f41163
Isaac <isaclopezcar@gmail.com>
2025-12-15 17:11:00 -0300

    Correcion task7 fuera de clase

--- task7.js
+++ task7.js
@@ -4,14 +4,32 @@ const prompt = require("prompt-sync")();
 const numero = prompt("Ingresa el numero limite del arreglo: ");
 const numeros = [];
 
+function Prime(prime) {
+
+  if (prime <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(prime); i++) {
+    if (prime % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+
+}
+
+
+
 function showPrimeOdd(numero) {
 
   for (let i = 1; i <= numero; i++) {
 
 
-    if (i > 1 && i % 2 !== 0 && i > 30) {
+    if (Prime(numero) && i % 2 !== 0 && i > 30) {
       numeros.push("BigPrimeOdd");
-    } else if (i > 1 && i % 2 !== 0) {
+    } else if (Prime(numero) && i % 2 !== 0) {
       numeros.push("PrimeOdd");
 
     } else {
